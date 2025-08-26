4dc5fa0381020c895fde87ac20baa130b2a2326f
RobertCalder1 <robertcalder27@hotmail.com>
2025-08-26 16:36:08 +1000

    Initial commit

---
+++ src/main/java/org/extstudios/mossControl/MossControl.java
@@ -0,0 +1,17 @@
+package org.extstudios.mossControl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MossControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
